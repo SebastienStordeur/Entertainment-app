409b4f06db715e5b731d1c089875a0094901226d
Sebastien Stordeur <stordeursebastien0@gmail.com>
2022-10-11 20:36:03 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1></h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
