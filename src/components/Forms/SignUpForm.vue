2552cf3311336213f0a40741fc70900fac0a8eb9
Sebastien Stordeur <stordeursebastien0@gmail.com>
2022-10-20 18:28:55 +0200

    Added full validation on signup form

--- src/components/Forms/SignUpForm.vue
+++ src/components/Forms/SignUpForm.vue
@@ -22,6 +22,12 @@
         "
         ref="email"
       />
+      <p v-if="emailHasError" class="font-bold text-red text-13">
+        {{ emailMsgError }}
+      </p>
+      <p v-if="emailIsUsed" class="font-bold text-red text-13">
+        Email already used
+      </p>
     </div>
     <div class="flex flex-col mt-6">
       <label
@@ -42,7 +48,11 @@
           h-9
         "
         ref="password"
+        autocomplete="off"
       />
+      <p v-if="passwordHasError" class="font-bold text-red text-13">
+        {{ passwordMsgError }}
+      </p>
     </div>
     <div class="flex flex-col mt-6">
       <label
@@ -64,7 +74,17 @@
           h-9
         "
         ref="repeatpsw"
+        autocomplete="off"
       />
+      <p v-if="confirmPswHasError" class="font-bold text-red text-13">
+        {{ confirmPswMsgError }}
+      </p>
+      <p
+        v-if="!confirmPswHasError && passwordsHaveError"
+        class="font-bold text-red text-13"
+      >
+        {{ passwordsMsgError }}
+      </p>
     </div>
     <button
       type="submit"
@@ -81,6 +101,9 @@
     >
       Create an account
     </button>
+    <div class="text-white flex justify-center" v-if="accountCreated">
+      Account created successfully
+    </div>
     <div class="flex justify-center text-15 mt-6 font-light">
       <p class="text-white">Already have an account?&nbsp;</p>
       <a href="/login" class="text-red cursor-pointer">Login</a>
@@ -95,31 +118,98 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 export default {
   data() {
-    return {};
+    return {
+      emailRegex:
+        /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i,
+      emailHasError: false,
+      passwordHasError: false,
+      confirmPswHasError: false,
+      passwordsHaveError: false,
+      emailIsUsed: false,
+      emailMsgError: "",
+      passwordMsgError: "",
+      confirmPswMsgError: "",
+      passwordsMsgError: "",
+      accountCreated: false,
+      auth: getAuth(),
+    };
   },
   methods: {
+    emailValidityHandler(email) {
+      if (email.trim() === "") {
+        this.emailHasError = true;
+        this.emailMsgError = "Can't be empty";
+      } else if (email.trim() !== "" && !this.emailRegex.test(email)) {
+        this.emailHasError = true;
+        this.emailMsgError = "Wrong email format";
+      } else {
+        this.emailHasError = false;
+        this.emailMsgError = "";
+      }
+    },
+    passwordValidityHandler(password) {
+      if (password.length < 6) {
+        this.passwordHasError = true;
+        this.passwordMsgError = "Password should contain at least 6 characters";
+      } else {
+        this.passwordHasError = false;
+        this.passwordMsgError = "";
+      }
+    },
+    confirmPasswordValidityHandler(confirmPassword) {
+      if (confirmPassword.length < 6) {
+        this.confirmPswHasError = true;
+        this.confirmPswMsgError =
+          "Password should contain at least 6 characters";
+      } else {
+        this.confirmPswHasError = false;
+        this.confirmPswMsgError = "";
+      }
+    },
+    passwordsEqualityHandler(password, confirmPassword) {
+      if (password !== confirmPassword) {
+        this.passwordsHaveError = true;
+        this.passwordsMsgError = "Passwords should be identical";
+      } else {
+        this.passwordsHaveError = false;
+        this.passwordsMsgError = "";
+      }
+    },
     handleSubmit() {
       const email = this.$refs.email.value;
       const password = this.$refs.password.value;
-      const repeatpsw = this.$refs.repeatpsw.value;
-      const auth = getAuth();
+      const confirmPassword = this.$refs.repeatpsw.value;
       const usersCollectionRef = collection(db, "users");
+      this.accountCreated = false;
 
-      if (password !== repeatpsw) return;
+      this.emailValidityHandler(email);
+      this.passwordValidityHandler(password);
+      this.confirmPasswordValidityHandler(confirmPassword);
+      this.passwordsEqualityHandler(password, confirmPassword);
 
-      createUserWithEmailAndPassword(auth, email, password).then(
-        async (user) => {
-          const userUid = user.user.uid;
-          const email = user.user.email;
-
-          console.log(userUid, email);
-          await setDoc(doc(usersCollectionRef, userUid), {
-            userUid,
-            email,
-            bookmarks: [],
+      if (
+        !this.emailHasError &&
+        !this.passwordHasError &&
+        !this.confirmPswHasError &&
+        !this.passwordsHaveError
+      ) {
+        createUserWithEmailAndPassword(this.auth, email, password)
+          .then(async (user) => {
+            const userUid = user.user.uid;
+            const email = user.user.email;
+            await setDoc(doc(usersCollectionRef, userUid), {
+              userUid,
+              email,
+              bookmarks: [],
+            });
+            this.accountCreated = true;
+          })
+          .catch((error) => {
+            error.message === "Firebase: Error (auth/email-already-in-use)."
+              ? (this.emailIsUsed = true)
+              : (this.emailIsUsed = false);
           });
-        }
-      );
+      }
     },
   },
 };
