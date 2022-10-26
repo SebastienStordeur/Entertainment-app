826f8185c494b645c2f69908b6a13dac41d2cecd
Sebastien Stordeur <stordeursebastien0@gmail.com>
2022-10-26 15:48:53 +0200

    Add protected routes guards for login, signup and bookmarks

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
+import { getAuth, onAuthStateChanged } from "@firebase/auth";
 
 import App from "./App.vue";
 import HomePage from "./pages/HomePage.vue";
@@ -9,25 +10,44 @@ import BookmarkPage from "./pages/BookmarkPage.vue";
 import SignUp from "./pages/SignUp.vue";
 import LoginPage from "./pages/LoginPage.vue";
 
+const auth = getAuth();
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", component: HomePage, name: "Home" },
     { path: "/movies", component: MoviesPage, name: "Movies" },
     { path: "/series", component: SeriesPage, name: "Series" },
-    { path: "/bookmarks", component: BookmarkPage, name: "Bookmarks" },
+    {
+      path: "/bookmarks",
+      component: BookmarkPage,
+      name: "Bookmarks",
+      beforeEnter: (to, from, next) => {
+        onAuthStateChanged(auth, async (user) => {
+          user ? next() : next({ name: "Home" });
+        });
+      },
+    },
     {
       path: "/signup",
       component: SignUp,
       name: "Signup",
-      beforeEnter: () => {
-        const isAuthenticated = false;
-        if (isAuthenticated) {
-          return { name: "Home" };
-        }
+      beforeEnter: (to, from, next) => {
+        onAuthStateChanged(auth, async (user) => {
+          user ? next({ name: "Home" }) : next();
+        });
       },
     },
-    { path: "/login", component: LoginPage, name: "Login" },
+    {
+      path: "/login",
+      component: LoginPage,
+      name: "Login",
+      beforeEnter: (to, from, next) => {
+        onAuthStateChanged(auth, async (user) => {
+          user ? next({ name: "Home" }) : next();
+        });
+      },
+    },
   ],
   linkActiveClass: "brightness-300",
 });
